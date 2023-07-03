6be86f641927a66b11729a987451dd05ce75e16e
Giacomo-Arcangeli <giacomoarcangeli1997@gmail.com>
2023-07-03 18:36:19 +0200

    add pokemon card from store

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,34 @@
 <script>
-export default {
+import axios from 'axios';
+const endpoint = 'https://41tyokboji.execute-api.eu-central-1.amazonaws.com/dev/api/v1/pokemons';
+import { store } from './store';
+import PokemonCard from './components/PokemonCard.vue';
 
+export default {
+  components: { PokemonCard },
+  created() {
+    axios.get(endpoint).then(res => {
+      store.pokemons = res.data.docs;
+    })
+  },
+  data() {
+    return { store }
+  }
 }
 </script>
 
 
 
-<template></template>
+<template>
+  <div class="container">
+    <h1 class="text-center my-4">Pokemon</h1>
+    <div class="row g-4 row-cols-5">
+      <div class="col" v-for="pokemon in store.pokemons" :key="pokemon.number">
+        <PokemonCard :name="pokemon.name" :type="pokemon.type1" :image="pokemon.imageUrl" />
+      </div>
+    </div>
+  </div>
+</template>
 
 
 
---
+++ src/components/PokemonCard.vue
@@ -0,0 +1,35 @@
+<script>
+export default {
+    props: {
+        name: String,
+        type: String,
+        image: String
+    }
+}
+</script>
+
+
+
+<template>
+    <div class="pok-card text-center mt-2">
+        <img :src="image" alt="" srcset="">
+        <h4 class="my-2">{{ name }}</h4>
+        <div>{{ type }}</div>
+    </div>
+</template>
+
+
+
+<style lang="scss">
+.pok-card {
+    padding: 10px;
+    background-color: azure;
+    box-shadow: 5px 5px 10px rgb(179, 179, 179);
+    border-radius: 10px;
+}
+
+img {
+    width: 100%;
+    height: 200px;
+}
+</style>
